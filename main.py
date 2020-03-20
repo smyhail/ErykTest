892e9ec082b217fc43340b44780616c72d6e0311
smyhail <smyhail@gmail.com>
2020-03-20 13:24:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def obliczVat(cenaNetto):
+    vat = 1.23
+    cenaBrutto = cenaNetto * vat
+    print(cenaBrutto)
+
+
+obliczVat(10)
+
+asdas = obliczVat(200)
+
+print(asdas)
---
+++ zadanieDomowev1.py
@@ -0,0 +1,40 @@
+# inicjuj zmienną która bedzie przechpwywac sume
+
+# iniciuj zmienną o nazwie..
+#
+# która bedzie przechowywac stan Twojej decyzji...
+# "T" jak bedziesz chiał kontynułować lub coś innego jak bedziesz chciał przerwac
+
+
+# pettla krtóra ebdzie wykonywana dopuku bedziesz TY chiał ;)
+
+    # Pobieranie wartości jako int z ODPOWIEDNIM pytaniem
+
+    # podsumuj wartość X oraz Wynik
+
+    # wydrukuj aktualną wartość z ODPOWIEDNIM komentarzem
+
+    # zapytaj czy kontynułować  w 2 liniach
+
+
+# wydrukuj wynik z ODPOWIEDNIM komentarzem
+
+
+#  A TERAZ INSTRUKCJE WARUNKOWE króre robiłeś wcześniej
+
+# jeżeli wartosc koncowa jest mniejsza od 150 to wydrujuj z ODPOWIEDNIM komentarzem
+
+
+# jeżeli wartość koncowa jest pomiedzy 150 i 200 to wydrujuj z ODPOWIEDNIM komentarzem
+
+
+# jeżeli wartość koncowa jest pomiedzy 150 i 200 to wydrujuj z ODPOWIEDNIM komentarzem
+
+
+# poinformuj o zakonczeniu dziłałąnia programu drukując magiczne angielskie dwa słowa, które  konczą filmy
+# ten patametr jest opcjonalny
+
+
+
+
+# PS. ODPOWIEDNIE znaczy ze nyc zadane słownie pytanie lub komentarz
